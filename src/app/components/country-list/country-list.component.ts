a6a2d171100e99fddae7e88b6f7c3f8159bbe069
Menna-Ashraf512 <mennaalla@gmail.com>
2025-07-11 15:26:17 +0300

    Refactor country list component for improved readability and maintainability

--- src/app/components/country-list/country-list.component.ts
+++ src/app/components/country-list/country-list.component.ts
@@ -18,6 +18,9 @@ export class CountryListComponent implements OnInit {
   filteredCountries: Country[] = [];
   searchTerm = '';
   selectedRegion = 'Filter by Region';
+  currentPage = 1;
+  itemsPerPage = 12;
+  paginatedCountries: Country[] = [];
 
   constructor(private countryService: CountryService) {}
 
@@ -28,6 +31,7 @@ export class CountryListComponent implements OnInit {
     this.countryService.getAllCountries().subscribe((countries) => {
       this.countries = countries;
       this.filteredCountries = countries;
+      this.setPaginatedCountries();
     });
   }
   onSearch() {
@@ -53,5 +57,38 @@ export class CountryListComponent implements OnInit {
     }
 
     this.filteredCountries = filtered;
+    this.currentPage = 1;
+    this.setPaginatedCountries();
+  }
+
+  setPaginatedCountries() {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+    this.paginatedCountries = this.filteredCountries.slice(start, end);
+  }
+
+  goToPage(page: number) {
+    this.currentPage = page;
+    this.setPaginatedCountries();
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.filteredCountries.length / this.itemsPerPage);
+  }
+
+  get displayedPages(): number[] {
+    const total = this.totalPages;
+    const current = this.currentPage;
+    const maxPages = 3;
+    let start = Math.max(1, current - 1);
+    let end = Math.min(total, start + maxPages - 1);
+    if (end - start < maxPages - 1) {
+      start = Math.max(1, end - maxPages + 1);
+    }
+    const pages = [];
+    for (let i = start; i <= end; i++) {
+      pages.push(i);
+    }
+    return pages;
   }
 }
